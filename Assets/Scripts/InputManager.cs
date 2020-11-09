4e7500fc433f2c448e675c2447a70f7db9530a2f
DrollestHook <nozdrinns@gmail.com>
2020-11-09 15:11:14 +0300

    First Commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditorInternal;
+
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public static InputManager Instance => _instance;
+    Vector2 _firstTouchPosition, _currentTouchPosition;
+
+    static InputManager _instance;
+
+    void Awake() {
+        _instance = this;
+    }
+
+    void Update()
+    {
+        CheckForInput();
+    }
+
+    public Vector2 GetFirstTouchPosition() {
+        return _firstTouchPosition;
+    }
+
+    public Vector2 GetCurrentTouchPosition() {
+        return _currentTouchPosition;
+    }
+
+    void CheckForInput() {
+        if (Input.GetMouseButtonDown(0)) {
+            _firstTouchPosition = Input.mousePosition;
+            print(_firstTouchPosition);
+        }
+    }
+    
+    
+}
